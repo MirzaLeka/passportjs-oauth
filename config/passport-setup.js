7fc026a1fee63317478d6c9a02c14b66f8e12e00
Mirza Leka <mirza_376@hotmail.com>
2020-08-13 16:33:34 +0200

    wrapping up serialization

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -2,6 +2,21 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const User = require('../models/user-model');
 
+// is invoked with done and ID is sent to the client
+passport.serializeUser((user, done) => {
+  done(null, user.id); // first param is error
+});
+
+// ID is sent from the client
+passport.deserializeUser((id, done) => {
+
+    // check if user with given id exist in db
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+
+});
+
 passport.use(
   new GoogleStrategy({
     callbackURL: '/auth/google/redirect', // custom made route
@@ -10,22 +25,32 @@ passport.use(
   }, (accessToken, refreshToken, profileData, done) => {
 
     // check if user exists in our db
-    User.findOne({ googleId: profileData.googleId }).then((currentUser) => {
+    User.findOne({ googleId: profileData.googleId }).then(currentUser => {
       if (currentUser) {
-
+        done((e) => {
+          if (e) {
+            console.log(e);
+          }
+        }, currentUser);
       } else {
-        registerNewUser(profileData);
+        registerNewUser(profileData, done);
       }
     });
 
   })
 )
 
-function registerNewUser(data) {
+function registerNewUser(data, done) {
   new User({
     username: data.displayName, // data coming from google API
     googleId: data.id
   }).save().then((newUser) => {
-    console.log('new user creatred ', newUser);
+
+    done((e) => {
+      if (e) {
+        console.log(e);
+      }
+     }, newUser);
+
   });
 }
